85209df2e0ac7cc28dfad40f2409e886d3dfebfe
mnick79 <mnick79@mail.ru>
2021-04-27 00:03:11 +0300

    Commited at 2021.04.27_1

--- first/women/urls.py
+++ first/women/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
     path('', index),
-    path('cats/', categories)
+    path('cats/<int:catid>/', categories),
+    re_path(r'^archive/(?P<year>[0-9]{4})/', archive),
+    path('boot/', boots)
+
 ]
--- first/women/views.py
+++ first/women/views.py
@@ -4,5 +4,14 @@ from django.http import HttpResponse
 def index(request):
     return HttpResponse("<h1>THe my first page</h1>")
 
-def categories(request):
-    return HttpResponse("<h1>The my second page</h1>")
+def categories(request, catid):
+    return HttpResponse(f"<h1>The my second page</h1><p>{catid*2}</p>")
+
+def archive(request, year):
+    if int(year) > 2021:
+        return HttpResponse(f'Вывод {year}')
+    return HttpResponse(f'PageNotFound!')
+
+def boots(request, method=['GET']):
+    print(f'" ".join([a+"="+b+"\n" for a,b in request.GET.items()])')
+    return HttpResponse(f'{"".join([a+"="+b+"  " for a,b in request.GET.items()])}')
